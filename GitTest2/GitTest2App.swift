5b281aba9658b4d8c49ff4d3e6120c79e25006b5
luc <luc.spielvogel@syntax-institut.de>
2023-06-01 10:42:12 +0200

    Initial Commit

---
+++ GitTest2/GitTest2App.swift
@@ -0,0 +1,17 @@
+//
+//  GitTest2App.swift
+//  GitTest2
+//
+//  Created by Luc Spielvogel on 01.06.23.
+//
+
+import SwiftUI
+
+@main
+struct GitTest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
